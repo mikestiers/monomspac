04ffd8a8dfa4b14716ca478e114c24e5569f07c8
Mike Stiers <mike.stiers@outlook.com>
2022-04-11 00:58:01 -0400

    added cell class

---
+++ Entities/Cell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryPOC
+{
+    class Cell
+    {
+        public Cell(int x, int y, bool isDot, bool visited)
+        {
+            this.squareSize = 12;
+            this.x = x;
+            this.y = y;
+            this.isDot = isDot;
+            this.visited = visited;
+        }
+        public int squareSize = 12;
+        public int x { get; set; }
+        public int y { get; set; }
+        public bool isDot { get; set; }
+        public bool visited { get; set; }
+    }
+}
